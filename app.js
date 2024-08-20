536ed00453c78980bb58e7dda71329b2505b93a2
ArcanistAasimar <mz2606@gmail.com>
2024-08-20 00:38:11 -0400

    Module 7 completed with known POST error

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ var apiRouter = require('./app_api/routes/index');
 
 var handlebars = require('hbs');
 
+// Wire in our authentication module 
+var passport = require('passport');
+require('./app_api/config/passport');
+
 // Brind in the database
 require('./app_api/models/db');
+
+// Pull contents of .env file
+require('dotenv').config();
 
 var app = express();
 
@@ -30,11 +37,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -46,12 +54,12 @@ app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -59,6 +67,15 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Catch unauthorized error and create 401 
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({ "message": err.name + ": " + err.message });
+  }
 });
 
 module.exports = app;
